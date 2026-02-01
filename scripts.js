6b71ec21e537e8fe1509e279d9b02829886c4cc1
satoshikkj <richardsadeboy@gmail.com>
2026-02-01 17:14:36 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function toggleDetalhes(button) {
+  const detalhes = button.nextElementSibling.nextElementSibling;
+
+  if (detalhes.style.display === "block") {
+    detalhes.style.display = "none";
+    button.textContent = "Ver Detalhes";
+  } else {
+    detalhes.style.display = "block";
+    button.textContent = "Ocultar Detalhes";
+  }
+}
